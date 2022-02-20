5dadb73eb40293d4c51bcb369611d1c415bea1be
Dan McAtee <danielmcatee@me.com>
2022-02-20 01:40:13 +0100

    add playGame logic

--- app.ts
+++ app.ts
@@ -1,5 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const rotateButton = document.getElementById("rotate") as Element;
+  const startButton = document.getElementById("start") as Element;
+  const info = document.getElementById("info") as Element;
+  const turn = document.getElementById("turn") as Element;
 
   const playerGrid = new PlayerGrid();
   const computerGrid = new ComputerGrid();
@@ -52,5 +55,46 @@ document.addEventListener("DOMContentLoaded", () => {
     const positionTuple = target.id.split("-").slice(1);
     const position: Coordinate = [positionTuple[0], parseInt(positionTuple[1])];
     playerGrid.placeShip(selectedShip, selectedShipPart, position);
+  }
+
+  function playGame(currentPlayer: "player" | "computer" = "player") {
+    turn.innerHTML = `${
+      currentPlayer === "computer" ? "Computers" : "Players"
+    } turn`;
+    if (currentPlayer === "player") {
+      computerGrid.squares.forEach((square) => {
+        square.addEventListener("click", () =>
+          revealSquare(square, currentPlayer)
+        );
+      });
+    } else {
+      const firedSquare = playerGrid.randomHit();
+      revealSquare(firedSquare, "computer");
+    }
+  }
+
+  startButton.addEventListener("click", () => playGame());
+
+  function revealSquare(square: Element, currentPlayer: "player" | "computer") {
+    let grid = currentPlayer === "player" ? computerGrid : playerGrid;
+    const [_, char, number] = square.id.split("-");
+    const position: Coordinate = [char, parseInt(number)];
+    const squareValue = grid.get(position);
+    if (shipNames.includes(squareValue as ShipType)) {
+      console.log("hit", squareValue);
+      const hitShip = grid.ships.find((ship) => ship.type === squareValue);
+      hitShip?.hit();
+      square.classList.add("boom");
+      grid.set(position, "hit");
+      if (hitShip?.sunken()) {
+        info.innerHTML = `${hitShip.type.toUpperCase()} sunken`;
+      }
+    } else if (!squareValue) {
+      console.log("miss");
+      square.classList.add("miss");
+      grid.set(position, "miss");
+    }
+
+    playGame(currentPlayer === "computer" ? "player" : "computer");
   }
 });
