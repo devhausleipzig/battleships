2e56ebf4b6a9df875310d911b6b81a724f5a12ff
Dan McAtee <danielmcatee@me.com>
2022-02-21 09:49:55 +0100

    format

--- grid.ts
+++ grid.ts
@@ -24,6 +24,7 @@ abstract class Grid {
       this.type === "player" ? "grid-player" : "grid-computer"
     );
   }
+
   protected makeKey(tuple: Coordinate): Key {
     const [char, number] = tuple;
     return `${char}-${number}`;
